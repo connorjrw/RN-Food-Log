6df27e8694ca92284a38b4aaeb4df0e185467f5e
connorjrw <connorjrw@hotmail.co.nz>
2020-12-15 21:35:35 +1300

    Flick between dates on home page

--- components/Home.js
+++ components/Home.js
@@ -1,33 +1,65 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
+
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const getCurrentDate=()=>{
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  var date = new Date().getDate();
-  var month = new Date().getMonth() + 1;
-  var year = new Date().getFullYear();
-  var day = new Date().getDay()
-  return date + ' ' + months[month - 1] + ' ' + year ;//format: dd-mm-yyyy;
+const formatDate = (date) => {
+    console.log(date)
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    var ddate = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear()
+    return ddate + ' ' + months[month - 1] + ' ' + year ;
 }
 
+const nextDate = (selectedDate) => {
+    var datevar = new Date(selectedDate)
+    datevar.setDate(datevar.getDate() + 1)
+    return formatDate(datevar)
+}
+const previousDate = (selectedDate) => {
+    var datevar = new Date(selectedDate)
+    datevar.setDate(datevar.getDate() -1)
+    return formatDate(datevar)
+}
+
+
 function Home() {
+  var today = new Date()
+  const [selectedDate, setDate] = useState(formatDate(today));
+  useFocusEffect(
+    React.useCallback(() => {
+      let isMounted = true; 
+      return () => {
+        isMounted = false
+        // Do something when the screen is unfocused
+        // Useful for cleanup functions
+      };
+    }, [])
+  );
   return (
    <View style = {styles.container}>
-       <Text style = {styles.date}>{getCurrentDate()}</Text>
+        <View style = {styles.datewrap}>
+          <View>
+            <TriangleLeft onPress = { () => { setDate(previousDate(selectedDate)) }}> </TriangleLeft>
+          </View>
+          <View>
+            <Text style = {styles.date}>{selectedDate}</Text></View>
+          <View>
+            <TriangleRight onPress = { () => { setDate(nextDate(selectedDate)) }}></TriangleRight>
+          </View>
+       </View>
    </View>
   );
 }
 
 export default function HomeStack({ navigation: { navigate } }){
-  console.log('nav',navigate)
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -54,6 +86,17 @@ export default function HomeStack({ navigation: { navigate } }){
   );
 }
 
+const Triangle = (props) => {
+  return <TouchableOpacity style={[styles.triangle, props.style]} onPress = {props.onPress}/>;
+};
+const TriangleRight = (props) => {
+  return <Triangle style =  {styles.triangleRight} onPress = {props.onPress}></Triangle>
+}
+const TriangleLeft = (props) => {
+  return <Triangle style = {styles.triangleLeft} onPress = {props.onPress}></Triangle>
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -65,6 +108,34 @@ const styles = StyleSheet.create({
   date:{
     alignSelf:'center',
     marginTop:10,
-    fontSize:30
+    fontSize:27
+  },
+  triangle: {
+    marginHorizontal:20,
+    marginTop:15,
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderLeftWidth: 12,
+    borderRightWidth: 12,
+    borderBottomWidth: 24,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "#293236",
+  },
+  triangleRight:{
+    transform: [{ rotate: "90deg" }],
+  },
+  triangleLeft:{
+    transform: [{ rotate: "-90deg" }],
+  },
+  datewrap:{
+    marginTop:20,
+    flex:1,
+    height:10,
+    alignItems:'flex-start',
+    justifyContent:'space-between',
+    flexDirection:'row'
   }
 });
